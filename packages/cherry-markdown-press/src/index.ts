9614d247bf3a58459018e4db18b1cebd14964f61
任帅 <1064425721@qq.com>
2023-12-21 22:48:16 +0800

    feat: read files under directory

--- packages/cherry-markdown-press/src/index.ts
+++ packages/cherry-markdown-press/src/index.ts
@@ -1,5 +1,38 @@
+import * as fs from 'fs';
 import { normalizePath } from './utils';
+import { join, basename, relative } from 'path';
+import { error } from 'console';
 
 const targetFilePath = normalizePath(process.cwd() + "/" + 'docs')
 
-console.log(targetFilePath)
+const checkCurrentDirectory = (targetFilePath: string): any[] => {
+  let directories: any[] = [];
+  try {
+    const currentDirectoryFiles = fs.readdirSync(targetFilePath);
+    currentDirectoryFiles.forEach((file) => {
+      const fullPath = join(targetFilePath.toString(), file);
+      const isDirectory = fs.statSync(fullPath).isDirectory();
+      if (isDirectory) {
+        directories = directories.concat(checkCurrentDirectory(fullPath));
+      } else if (fullPath.endsWith('.md')) {
+        const fileName = basename(fullPath, '.md');
+        const relativePath = normalizePath(relative(process.cwd(), fullPath)).replace('.md', '').replace('docs/', '');
+        const fileObject = {
+          [relativePath]: {
+            name: fileName,
+            path: fullPath,
+
+          }
+        };
+        directories.push(fileObject);
+      }
+    });
+  } catch (e) {
+    console.error(e);
+    throw error(e);
+  }
+  return directories;
+}
+
+const directories = checkCurrentDirectory(targetFilePath);
+console.log(JSON.stringify(directories, null, 2));
