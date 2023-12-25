52464f19a1a1de93139138ed04ddcbbd54ba400c
Jimmy <jimmyrss1102@gmail.com>
2023-12-26 01:57:45 +0800

    feat: use`cherry-markdown` mark html

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -1,12 +1,19 @@
 import fs from 'fs'
 import { Plugin } from 'vite'
 import { checkCurrentDirectory } from './utils'
+const { default: CherryEngine } = require('cherry-markdown/dist/cherry-markdown.engine.core.common');
+
+const cherryEngineInstance = new CherryEngine({});
+
 export default function vitePluginCherryMarkdown(): Plugin {
   return {
     name: 'vite-plugin-cherry-markdown',
     transform(code, id) {
       const directories = checkCurrentDirectory("./docs");
-      console.log(JSON.stringify(directories, null, 2));
+      // console.log(JSON.stringify(directories, null, 2));
+
+      const html = cherryEngineInstance.makeHtml("## Hello World");
+      console.log(html);
     },
   }
 }
